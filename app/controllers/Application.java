f36e90571fc64fd8325eebf34266518328b7def1
rgalarcia <ramongarciaalarcia@gmail.com>
2019-01-21 04:29:58 +0100

    Preliminar version

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,6 +2,7 @@ package controllers;
 
 import groovy.json.internal.JsonParserUsingCharacterSource;
 import groovy.json.internal.JsonStringDecoder;
+import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
 import play.*;
 import play.mvc.*;
@@ -31,6 +32,18 @@ public class Application extends Controller {
         render(username, param);
     }
 
+    public static void account(String message, String username){
+        User us = User.find("byUsername", username).first();
+        String email = us.email;
+        int mobil = us.mobil;
+        if (message == null) {
+            message = "Update your username data here.";
+            render(message, username, email, mobil);
+        } else {
+            render(message, username, email, mobil);
+        }
+    }
+
     public static void profileAndroid(String u){
 
         User us = User.find("byUsername", u).first();
@@ -40,22 +53,97 @@ public class Application extends Controller {
 
     public static void api(String param){
         List<Monument> monList;
+        List<String> paramList = Arrays.asList(param.split(","));
 
         if(param != null) {
-            monList = Monument.find("byParam", param).fetch();
-
-        }
-        else {
+            monList = Monument.find("Param in (?1)", paramList).fetch();
+        } else {
             monList = Monument.findAll();
         }
         renderJSON(monList);
     }
 
-    public static void doregister (String u, String e, String p, Integer m, String g)
+    public static void likemonument (String usr, String mon){
+        User username = User.find("byUsername", usr).first();
+        Monument monument = Monument.find("byName", mon).first();
+        List<Monument> listMon = username.lmonuments;
+        listMon.add(monument);
+
+        username.lmonuments = listMon;
+        username.save();
+        profile(usr);
+    }
+
+    public static void dislikemonument (String usr, String mon){
+        User username = User.find("byUsername", usr).first();
+        Monument monument = Monument.find("byName", mon).first();
+        List<Monument> listMon = username.lmonuments;
+        listMon.remove(monument);
+
+        username.lmonuments = listMon;
+        username.save();
+        profile(usr);
+    }
+
+    public static void isliked(String usr, String mon){
+        int isliked = 0;
+
+        User username = User.find("byUsername", usr).first();
+        Monument monument = Monument.find("byName", mon).first();
+
+        for (Monument m:username.lmonuments){
+            if(m.name.equals(monument.name)){
+                isliked = 1;
+            }
+        }
+
+        renderText(isliked);
+    }
+
+    public static void accountmanager (Integer a, String u, String e, String p, String np, Integer m, String g)
     {
-        User us = new User(u, e, p, m, g).save();
-        us.save();
-        index("Registered correctly. :)");
+        if (a == 0) {
+            List<User> usersList = User.find("byUsername", u).fetch();
+
+            if (usersList.size() == 0) {
+                int count = StringUtils.countMatches(g, ",");
+
+                if (count != 0) {
+                    g = g.replaceAll("\\s+", "");
+                    User us = new User(u, e, p, m, g).save();
+                    us.save();
+                } else {
+                    User us = new User(u, e, p, m, g).save();
+                    us.save();
+                }
+
+                index("Registered correctly. :)");
+            } else {
+                index("ERROR: The username already exists. :(");
+            }
+        } else if (a == 1){
+            User us = User.find("byUsernameAndPassword", u, p).first();
+
+            if (us == null) {
+                account("ERROR: Username and password do not match. :(", u);
+            } else {
+                int count = StringUtils.countMatches(g, ",");
+
+                if (count != 0) {
+                    g = g.replaceAll("\\s+", "");
+                }
+
+                us.username = u;
+                us.email = e;
+                us.password = np;
+                us.mobil = m;
+                us.gustos = g;
+                us.save();
+
+                index("User data has been updated. Please log in again. :)");
+            }
+
+        }
     }
 
     public static void login (String u, String p)
@@ -71,16 +159,26 @@ public class Application extends Controller {
         }
     }
 
-
-
     public static void logout(){
         index("Welcome to MonumentFinder.");
+    }
+
+    public static void delete (String message, String username){
+        if(message == null) {
+            message = "Please introduce your password.";
+        }
+        render(message, username);
     }
 
     public static void removeUser (String u, String p)
     {
         User us = User.find("byUsernameAndPassword", u, p).first();
-        us.delete();
+        if (us == null) {
+            delete("ERROR: The password is incorrect.", u);
+        } else {
+            us.delete();
+            index("Your username was removed.");
+        }
     }
 
 
